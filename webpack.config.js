b6490e8014fa810f37d1092f957e56007941bbc6
Sarah <prosarah10@gmail.com>
2020-06-15 15:33:42 +0200

    Feat: Configure webpack

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require("path");
+
+module.exports = {
+    mode: 'development',
+    entry: path.resolve(__dirname, 'src','app'),
+    output: {
+        path: path.resolve(__dirname,'dist'),
+        filename: 'bundle.js',
+        publicPath: '/',
+    },
+    resolve: {
+        extensions: ['.js','.jsx']
+    },
+    devServer: {
+        historyApiFallback: true
+    },
+    module: {
+        rules: [{
+            test: /\.jsx?/,
+            loader:'babel-loader'
+        }]
+    }
+}
