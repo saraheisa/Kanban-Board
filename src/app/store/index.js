b3250caa3ded39a39b1c389be49af2aad05f83d5
Sarah <prosarah10@gmail.com>
2020-06-15 16:21:43 +0200

    Feat: Create a redux store

---
+++ src/app/store/index.js
@@ -0,0 +1,8 @@
+import { createStore } from 'redux';
+import { defaultState } from '../../server/defaultState';
+
+export const store = createStore(
+    function reducer (state = defaultState, action) {
+        return state;
+    }
+);
