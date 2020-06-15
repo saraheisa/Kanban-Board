b593535b39cc7c1a012d66efa6a83730eba64661
Sarah <prosarah10@gmail.com>
2020-06-15 17:31:25 +0200

    Feat: Connect TrackList to Dashboard

--- src/app/components/dashboard.jsx
+++ src/app/components/dashboard.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { connect } from "react-redux";
+import { ConnectedTaskList } from "./taskList";
 
 export const Dashboard = ({groups})=> (
     <div>
         <h2>Dashboard</h2>
         {groups.map (group => (
             <div>
-                {group.name}
+                <ConnectedTaskList id={group.id} name={group.name}/>
             </div>
         ))}
     </div>
