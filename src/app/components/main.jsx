acb2778f2380ae931fceb3d1bbf7a76e3d5351ac
Sarah <prosarah10@gmail.com>
2020-06-15 20:04:41 +0200

    Feat: Add routing

--- src/app/components/main.jsx
+++ src/app/components/main.jsx
@@ -2,11 +2,19 @@ import React from "react";
 import { Provider } from "react-redux";
 import { store } from '../store';
 import { ConnectedDashboard } from "./dashboard";
+import { Router, Route } from "react-router-dom";
+import { history } from "../store/history";
 
 export const Main = ()=> (
-    <Provider store={store}>
-        <div>
-            <ConnectedDashboard/>
-        </div>
-    </Provider>
+    <Router history={history}>
+        <Provider store={store}>
+            <div>
+                <Route
+                    exact
+                    path="/dashboard"
+                    render={() => <ConnectedDashboard/>}
+                />
+            </div>
+        </Provider>
+    </Router>
 )
