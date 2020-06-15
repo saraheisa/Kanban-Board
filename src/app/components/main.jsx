70924669bcd64d752ecb28a1a072bddfee96bc06
Sarah <prosarah10@gmail.com>
2020-06-15 17:02:42 +0200

    Feat: Connect dashboard to main component

--- src/app/components/main.jsx
+++ src/app/components/main.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { Provider } from "react-redux";
 import { store } from '../store';
+import { ConnectedDashboard } from "./dashboard";
 
 export const Main = ()=> (
     <Provider store={store}>
         <div>
-            Dashboards Goes Here!
+            <ConnectedDashboard/>
         </div>
     </Provider>
 )
