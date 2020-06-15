e032ed0e5ad4ff1fb07efa39d2c3e8313df32769
Sarah <prosarah10@gmail.com>
2020-06-15 16:51:29 +0200

    Feat: Rename file to .jsx and render the main component

---
+++ src/app/index.jsx
@@ -0,0 +1,6 @@
+import { store } from './store';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Main } from './components/main';
+
+ReactDOM.render(<Main/>, document.getElementById('app'));
