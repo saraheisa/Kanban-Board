a3d4557eb60c8e5acde7c35f6b5fabc010949003
Sarah <prosarah10@gmail.com>
2020-06-15 17:12:31 +0200

    Feat: Print group names

--- src/app/components/dashboard.jsx
+++ src/app/components/dashboard.jsx
@@ -4,6 +4,11 @@ import { connect } from "react-redux";
 export const Dashboard = ({groups})=> (
     <div>
         <h2>Dashboard</h2>
+        {groups.map (group => (
+            <div>
+                {group.name}
+            </div>
+        ))}
     </div>
 )
 
