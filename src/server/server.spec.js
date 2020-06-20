4c24d8bc92750b19d941f34517e4bddafdd031b0
Sarah <prosarah10@gmail.com>
2020-06-20 03:05:22 +0200

    Test: test the update task function

--- src/server/server.spec.js
+++ src/server/server.spec.js
@@ -1,9 +1,16 @@
-import { addNewTask } from './server';
+import { addNewTask, updateTask } from './server';
 
-addNewTask({
-    "name": "Finish React Course",
-    "id": "T4",
-    "group": "G2",
-    "owner": "U1",
-    "isComplete": false
-});
+(async function testServer() {
+    await addNewTask({
+        "name": "Finish React Course",
+        "id": "123",
+        "group": "G2",
+        "owner": "U1",
+        "isComplete": false
+    });
+
+    await updateTask({
+        "id": "T4",
+        "name": "Finish React Tutorial"
+    });
+})();
