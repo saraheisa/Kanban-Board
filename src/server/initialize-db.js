aebc16d2ca2ffdc457ed4fd6f0666d0afedf272c
Sarah <prosarah10@gmail.com>
2020-06-20 02:17:56 +0200

    Feat: seed DB

---
+++ src/server/initialize-db.js
@@ -0,0 +1,12 @@
+import { defaultState } from './defaultState';
+import { connectedDB } from './connect-db';
+
+async function initializeDB() {
+    let db = await connectedDB();
+    for (const collectionName in defaultState) {
+        let collection = db.collection(collectionName);
+        await collection.insertMany(defaultState[collectionName]);
+    }
+}
+
+initializeDB();
