574a2843b4dd80c7c4536613613a077e9f143305
Sarah <prosarah10@gmail.com>
2020-06-21 00:07:42 +0200

    Feat: create login route

--- src/app/components/main.jsx
+++ src/app/components/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Provider } from "react-redux";
 import { store } from '../store';
 import { ConnectedDashboard } from "./dashboard";
+import { ConnectedLogin } from "./login";
 import { Router, Route } from "react-router-dom";
 import { history } from "../store/history";
 import { ConnectedNavigation } from "./navigation";
@@ -21,6 +22,11 @@ export const Main = ()=> (
         <Provider store={store}>
             <div>
                 <ConnectedNavigation/>
+                <Route
+                    exact
+                    path="/"
+                    component={ ConnectedLogin }
+                />
                 <Route
                     exact
                     path="/dashboard"
