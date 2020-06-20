c2a5dd8e009b93f601f58a063b30c803cdcad423
Sarah <prosarah10@gmail.com>
2020-06-21 00:07:00 +0200

    Feat: display the authentication status

--- src/app/components/login.jsx
+++ src/app/components/login.jsx
@@ -2,18 +2,24 @@ import React from "react";
 import { connect } from "react-redux";
 import * as mutations from '../store/mutations';
 
-export const Login = ( { authenticateUser } ) => (
+export const Login = ( { authenticateUser, authenticated } ) => (
     <div>
         <h2>LOGIN!!!</h2>
         <form onSubmit={authenticateUser}>
             <input type="text" name="username" id="username" placeholder="Username"/>
             <input type="password" name="password" id="password" placeholder="password"/>
-            <input type="submit" value="LOGIN"/>
+            <input type="submit" value="LOGIN" disabled={ authenticated === mutations.AUTHENTICATING } />
+            {authenticated === mutations.NOT_AUTHENTICATED
+            ? <p>Username or Password is incorrect</p>
+            : null
+            }
         </form>
     </div>
 );
 
-const mapStateToProps = state => state;
+const mapStateToProps = ({ session }) => ({
+    authenticated: session.authenticated
+});
 
 const mapDispatchToProps = (dispatch) => ({
     authenticateUser(e) {
