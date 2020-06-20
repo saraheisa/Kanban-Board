f8b372d42fd61dd4c0051d282693f64c07b00eb2
Sarah <prosarah10@gmail.com>
2020-06-20 22:35:20 +0200

    Feat: add passwords to users in default state

--- src/server/defaultState.js
+++ src/server/defaultState.js
@@ -1,11 +1,15 @@
+import md5 from 'md5';
+
 export const defaultState = {
     users:[{
         id:"U1",
         name:"Dev",
+        passwordHash:md5("TUPLES"),
         friends:[`U2`]
     },{
         id:"U2",
         name:"C. Eeyo",
+        passwordHash:md5("PROFITING"),
         friends:[]
     }],
     groups:[{
